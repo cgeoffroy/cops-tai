04f9d12b1d88dc6624f3b4c88cb270261ba3a1c1
cgeoffroy <cgeoffroy@tai.com>
2013-06-21 15:10:14 +0200

    adding a new 'placement' resource

---
+++ src/main/java/org/tai/cops/minimal/PlacementResource.java
@@ -0,0 +1,17 @@
+package org.tai.cops.minimal;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Path("/placement")
+public class PlacementResource {
+	@GET
+	@Produces("text/occi")
+	@Path("{who}")
+	public Response sayGreeting(@PathParam("who") String name) {
+		return	Response.status(201).entity("Greetings, " + name + "!").build();
+	}
+}
