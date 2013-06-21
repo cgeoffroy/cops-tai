dad8ea9b1feefcc1dc0ed1bb965be1faa1de3885
cgeoffroy <cgeoffroy@tai.com>
2013-06-21 17:50:23 +0200

    starting to use a jersey client and using a occi antlr parser

--- src/main/java/org/tai/cops/minimal/Main.java
+++ src/main/java/org/tai/cops/minimal/Main.java
@@ -2,6 +2,8 @@ package org.tai.cops.minimal;
 
 
 import java.util.EnumSet;
+import java.util.List;
+import java.util.Map.Entry;
 
 import javax.servlet.DispatcherType;
 
@@ -10,10 +12,18 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 
 import com.google.inject.servlet.GuiceFilter;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+import occi.lexpar.OcciParser;
+
 /**
  * Starts an embedded Jetty server.
  */
 public class Main {
+	public static final String PUBLISHER_URL = "http://127.0.0.1:8086";
+	
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
         ServletContextHandler root = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS);
@@ -22,6 +32,27 @@ public class Main {
         root.addFilter(GuiceFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST));
         root.addServlet(EmptyServlet.class, "/*");
 
+        Client client = Client.create();
+        
+		WebResource webResource = client.resource(PUBLISHER_URL + "/-/");
+		System.out.println("Main.main() connecting to publisher");
+		ClientResponse response = webResource.accept("text/occi").type("text/occi")
+                   .get(ClientResponse.class);
+        System.out.println("Main.main() got a response");
+		
+		StringBuilder sb = new StringBuilder();
+		for (Entry<String, List<String>> k : response.getHeaders().entrySet()) {
+			if (! "Category".equals(k.getKey()))
+				continue;
+			//System.out.println("Main.main() " + k.getKey() + ": " + k.getValue());
+			for (String v : k.getValue()) {
+				sb.append(k.getKey()).append(": ").append(v).append('\n');
+			}
+		}
+		System.out.println("Main.main() : " + sb.toString());
+		System.out.println("response = " +
+				OcciParser.getParser(sb.toString()).category().toString());
+		
         server.start();
     }
 }
