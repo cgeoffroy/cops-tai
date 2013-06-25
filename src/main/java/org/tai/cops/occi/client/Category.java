de144725f793e148598e464725fe0fb23f89bbd5
cgeoffroy <cgeoffroy@tai.com>
2013-06-25 16:37:42 +0200

    moving field to final adn generate getter

--- src/main/java/org/tai/cops/occi/client/Category.java
+++ src/main/java/org/tai/cops/occi/client/Category.java
@@ -13,11 +13,11 @@ public class Category {
 	private final @Nonnull String term;
 	private final @Nonnull String claz;
 	
-	private String title;
-	private TypeIdentifier rel;
-	private URI location;
-	private String attributes;
-	private String actions;
+	private final String title;
+	private final TypeIdentifier rel;
+	private final URI location;
+	private final String attributes;
+	private final String actions;
 	
 	
 	private Category(@Nonnull String term, @Nonnull URI scheme, @Nonnull String claz,
@@ -31,6 +31,46 @@ public class Category {
 		this.location = location;
 		this.attributes = attributes;
 		this.actions = actions;
+	}
+
+
+	public URI getScheme() {
+		return scheme;
+	}
+
+
+	public String getTerm() {
+		return term;
+	}
+
+
+	public String getClaz() {
+		return claz;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public TypeIdentifier getRel() {
+		return rel;
+	}
+
+
+	public URI getLocation() {
+		return location;
+	}
+
+
+	public String getAttributes() {
+		return attributes;
+	}
+
+
+	public String getActions() {
+		return actions;
 	}
 	
 	/*public Category(Map<String, String> parsedValues) throws URISyntaxException {
